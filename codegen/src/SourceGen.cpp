a24ad94b327b79cfd1159a049a9ddc9bfe4ee622
pengubow <66965109+pengubow@users.noreply.github.com>
2025-02-28 12:02:41 +0200

    Update SourceGen.cpp

--- codegen/src/SourceGen.cpp
+++ codegen/src/SourceGen.cpp
@@ -3,6 +3,7 @@
 namespace { namespace format_strings {
 	constexpr char const* source_start = R"GEN(
 #include <stdexcept>
+#include <algorithm>
 #include <Geode/Bindings.hpp>
 #include <Geode/utils/addresser.hpp>
 #include <Geode/modify/Traits.hpp>
